9613a5a3d0ffc70db656c23e9b611359c81612e6
tknhs <kdsnlv0109@gmail.com>
2014-09-22 21:16:32 +0900

    fix: beacon same uuid, major, minor

--- beacon.go
+++ beacon.go
@@ -1,15 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"os/exec"
+	"strconv"
+	"time"
 )
 
-func BeaconScan() {
-	beacon, err := exec.Command("node", "path/to/reciever.js").Output()
-	if err != nil {
-		log.Fatal(err)
+func BeaconScan(beaconTimestamp chan string, beaconConfig BluetoothConfig) {
+	isBeacon := true
+
+	for isBeacon {
+		beaconInfo, err := exec.Command("node", "path/to/reciever.js").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var bc BluetoothConfig
+		err = json.Unmarshal(beaconInfo, &bc)
+		if err != nil {
+			log.Fatalf("can't decode json\n", err)
+		}
+
+		if beaconConfig == bc {
+			isBeacon = false
+		}
 	}
-	fmt.Println(string([]byte(beacon)))
+
+	t := time.Now().Unix()
+	beaconTimestamp <- strconv.Itoa(int(t))
 }
